f985ccff186fe23f8ed4a22d7431750140053861
yuchel222 <youyoumoremore811@gmail.com>
2024-12-29 14:52:44 +0900

    Add files via upload

--- kadai_sort.php
+++ kadai_sort.php
@@ -13,8 +13,10 @@
         { 
             if ($order === TRUE) {
                 sort($array);
+                echo '昇順にソートします<br>';
             } elseif ($order === FALSE) {
                 rsort($array);
+                echo '降順にソートします<br>';
             }
             foreach ($array as $value) {
                 echo "{$value}<br>" ;
@@ -23,10 +25,10 @@
        // ソートする配列を宣言
        $nums = [15, 4, 18, 23, 10];
        
-       echo '昇順にソートします<br>';
+       
        sort_2way($nums,TRUE);
        echo "<br>";
-       echo '降順にソートします<br>';
+     
        sort_2way($nums,FALSE);
        echo "<br>";       
         ?>
